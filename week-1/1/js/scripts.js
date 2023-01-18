50c1986a12ecfee5400b302e73abd68b61620959
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-01-18 13:35:41 +0100

    refactor: make minor changes

--- week-1/1/js/scripts.js
+++ week-1/1/js/scripts.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function(){
 
 	function filterInput () {
 		let str = '';
-		let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+		const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 		for (let symbol of this.value) {
 			if(numbers.includes(symbol)) {
 				str += symbol;
