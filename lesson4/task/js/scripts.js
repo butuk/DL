d36ce0e136e4d4d1f70383d38f3bb632173640a6
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-01-30 13:02:09 +0100

    feat: make task 1 for lesson4

--- lesson4/task/js/scripts.js
+++ lesson4/task/js/scripts.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', function(){
 	let menu = document.querySelector('.menu');
+	let toTop = document.querySelector('.scroll-to-top');
+	let content = document.querySelector('.content');
+
+	displayMoveButton(toTop, content, 22);
 	
 	if(window.location.hash.length > 0){
 		let link = menu.querySelector(`a[href$="${window.location.hash}"]`);
@@ -13,6 +17,13 @@ window.addEventListener('load', function(){
 		e.preventDefault();
 		applyMenuLinkClick(menu, e.target);
 	});
+
+	toTop.addEventListener('click', () => {
+		scrollWindow(0, true);
+	})
+	window.addEventListener('scroll', () => {
+		displayMoveButton(toTop, content, 22);
+	})
 });
 
 function applyMenuLinkClick(menu, link, animated = true){
@@ -28,11 +39,24 @@ function applyMenuLinkClick(menu, link, animated = true){
 		
 		link.classList.add(activeClass);
 
-		window.scrollTo({
-			top: target.offsetTop - 70,
-			behavior: animated ? 'smooth' : 'auto'
-		});
+		scrollWindow((target.offsetTop - 70), animated);
 	}
+}
+
+function scrollWindow(point, animated) {
+	window.scrollTo({
+		top: point,
+		behavior: animated ? 'smooth' : 'auto'
+	});
+}
+
+function displayMoveButton(button, content, position) {
+	let contentPosition = content.getBoundingClientRect().top;
+	if(contentPosition < position) {
+		button.style.display = 'flex';
+	} else {
+		button.style.display = 'none';
+	};
 }
 
 function delegate(box, eventname, selector, fn){
