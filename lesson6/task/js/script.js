887528b1b70019786852aae49b1c824835875fc5
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-02-06 11:58:36 +0100

    feat: make lesson-6 task

--- lesson6/task/js/script.js
+++ lesson6/task/js/script.js
@@ -1,40 +1,50 @@
 window.addEventListener('load', function(){
-
-	let btnPrev = document.querySelector('.gallery .buttons .prev');
-	let btnNext = document.querySelector('.gallery .buttons .next');
-
-	let images = document.querySelectorAll('.gallery .photos img');
-	let i = 0;
-
-	btnPrev.addEventListener('click', function(){
-		images[i].classList.remove('showed');
-		i--;
-
-		if(i < 0){
-			i = images.length - 1;
-		}
-
-		images[i].classList.add('showed');
-	});
-
-	btnNext.addEventListener('click', function(){
-		images[i].classList.remove('showed');
-		i++;
-
-		if(i >= images.length){
-			i = 0;
-		}
-
-		images[i].classList.add('showed');
-	});
-
-});
-
-/*
 	new Slider('.gallery-1');
+
 	let s2 = new Slider('.gallery-2');
 
 	setInterval(function(){
 		s2.next();
 	}, 3000)
-*/
+});
+
+class Slider {
+	constructor(selector) {
+		this.slider = document.querySelector(selector);
+		this.btnPrev = this.slider.querySelector('.buttons .prev');
+		this.btnNext = this.slider.querySelector('.buttons .next');
+		this.images = this.slider.querySelectorAll('.gallery .photos img');
+
+		this.imageNumber = 0;
+
+		this.btnNext.addEventListener('click', () => {
+			this.next();
+		});
+		this.btnPrev.addEventListener('click', () => {
+			this.previous();
+		});
+	}
+
+	previous() {
+		this.images[this.imageNumber].classList.remove('showed');
+		this.imageNumber--;
+
+		if(this.imageNumber < 0){
+			this.imageNumber = this.images.length - 1;
+		}
+
+		this.images[this.imageNumber].classList.add('showed');
+	}
+
+	next() {
+		this.images[this.imageNumber].classList.remove('showed');
+		this.imageNumber++;
+
+		if(this.imageNumber >= this.images.length){
+			this.imageNumber = 0;
+		}
+
+		this.images[this.imageNumber].classList.add('showed');
+	}
+}
+
